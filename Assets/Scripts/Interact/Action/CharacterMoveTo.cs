6100c63eb9d1e5fd4ad3132b82ad16c1adea2532
Svenpai <svenjanssen12321@gmail.com>
2018-02-22 23:26:54 +0100

    cleaned up spritesheet

---
+++ Assets/Scripts/Interact/Action/CharacterMoveTo.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveTo : Action
+{
+    //Characterlist
+    public List<Movement> CharacterList;
+
+    public List<GameObject> MoveLocationList;
+    public List<MoveOptions> MoveOptionList;
+
+    private List<bool> ActiveList;
+
+    // Use this for initialization
+    void Start()
+    {
+        ActiveList = new List<bool>();
+        foreach (Movement m in CharacterList)
+        {
+            ActiveList.Add(true);
+        }
+    }
+
+    public override void StartProcess()
+    {
+        Active = true;
+        StartCoroutine(MoveCharacters());
+    }
+
+    IEnumerator MoveCharacters()
+    {
+        while (Active)
+        {
+            for (int i = 0; i < CharacterList.Count; i++)
+            {
+                if (ActiveList[i])
+                {
+                    CheckMove(i);
+                }
+            }
+
+            EveryoneMoved();
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    void CheckMove(int index)
+    {
+        if (CharacterList[index].transform.position != MoveLocationList[index].transform.position)
+        {
+            if (MoveOptionList[index] == MoveOptions.Diagonal)
+            {
+                CharacterList[index].MoveTo(MoveLocationList[index].transform.position);
+            }
+
+            if (MoveOptionList[index] == MoveOptions.X_then_Y)
+            {
+                if (CharacterList[index].transform.position.x != MoveLocationList[index].transform.position.x)
+                {
+                    MoveX(index);
+                }
+                else
+                {
+                    MoveY(index);
+                }
+            }
+
+            if (MoveOptionList[index] == MoveOptions.Y_then_X)
+            {
+                if (CharacterList[index].transform.position.y != MoveLocationList[index].transform.position.y)
+                {
+                    MoveY(index);
+                }
+                else
+                {
+                    MoveX(index);
+                }
+            }
+        }
+        else
+        {
+            ActiveList[index] = false;
+        }
+    }
+
+    void MoveX(int index)
+    {
+        CharacterList[index].MoveTo(
+            new Vector2(
+                MoveLocationList[index].transform.position.x,
+                CharacterList[index].transform.position.y));
+    }
+
+    void MoveY(int index)
+    {
+        CharacterList[index].MoveTo(
+            new Vector2(
+                CharacterList[index].transform.position.x,
+                MoveLocationList[index].transform.position.y));
+    }
+
+    void EveryoneMoved()
+    {
+        bool FinalState = true;
+
+        foreach (bool state in ActiveList)
+        {
+            if (state == true)
+            {
+                FinalState = false;
+            }
+        }
+
+        if (FinalState == true)
+        {
+            Active = false;
+        }
+    }
+}
