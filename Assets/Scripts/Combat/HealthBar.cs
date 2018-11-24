b2521175e574d5dac3e85252ebedefc43eb2c518
Unknown <svenjanssen12321@gmail.com>
2018-11-24 23:39:27 +0100

    Added health bar to player
    Game still needs to track a health value

--- Assets/Scripts/Combat/HealthBar.cs
+++ Assets/Scripts/Combat/HealthBar.cs
@@ -1,18 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-
+    public Transform Bar;
+    private Image[] Images;
 
 	// Use this for initialization
 	void Start ()
     {
-	
+        Images = GetComponentsInChildren<Image>();
+        Bar.localPosition = new Vector2(0, 0);
+        UpdateUI();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void UpdateUI()
+    {
+        SetBars(1);
+        //25 needs to be health value
+        Bar.localPosition = new Vector2(25 - 100, 0);
+
+        StartCoroutine(_VisibilityTimer());
+    }
+
+    IEnumerator _VisibilityTimer()
+    {
+        yield return new WaitForSeconds(0.25f);
+
+        while (Images[0].color.a > 0)
+        {
+            SetBars(-0.2f);
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+    
+    void SetBars(float alpha)
+    {
+        Color temp = new Color(0, 0, 0, alpha);
+        foreach (Image i in Images)
+        {
+            i.color += temp;
+        }
+    }
 }
