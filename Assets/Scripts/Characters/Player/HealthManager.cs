63f902d95fee9982ba345b9d6d8b97e9298e19c5
Unknown <svenjanssen12321@gmail.com>
2018-11-25 16:51:07 +0100

    Improved healthbar
    Still needs savefile health

---
+++ Assets/Scripts/Characters/Player/HealthManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public HealthBar Healthbar;
+
+    private int Health; //A number between 0 & 100
+    public int health; //temp
+
+    public delegate void HealthChange(int health);
+    public event HealthChange OnHealthChange;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //needs to take health out of save file
+        Health = 100;
+        Healthbar.Link(this);
+        OnHealthChange(Health);
+	}
+	
+	//temp
+	void Update () {
+		if (health != Health)
+        {
+            Health = health;
+            OnHealthChange(Health);
+        }
+	}
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        if (Health <= 0)
+        {
+            //game over
+        }
+        else
+        {
+            OnHealthChange(Health);
+        }
+    }
+}
--- Assets/Scripts/Combat/HealthBar.cs
+++ Assets/Scripts/Combat/HealthBar.cs
@@ -1,44 +1,118 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
 
 public class HealthBar : MonoBehaviour
 {
-    public Transform Bar;
-    private Image[] Images;
+    public List<Image> Bar;
+    [Space]
+    public Image HealthIndicator;
+    public Image DamageIndicator;
+    public Image HealIndicator;
 
-	// Use this for initialization
-	void Start ()
+    private List<Transform> Bounds;
+    private int CurrentHealth;
+    private bool Fading;
+
+    // Use this for initialization
+    void Awake()
     {
-        Images = GetComponentsInChildren<Image>();
-        Bar.localPosition = new Vector2(0, 0);
-        UpdateUI();
+        Bounds = new List<Transform>();
+        foreach(Transform bound in transform.GetChild(1))
+        {
+            Bounds.Add(bound);
+        }
 	}
 
-    void UpdateUI()
+    public void Link(HealthManager manager)
     {
-        SetBars(1);
-        //25 needs to be health value
-        Bar.localPosition = new Vector2(25 - 100, 0);
-
-        StartCoroutine(_VisibilityTimer());
+        manager.OnHealthChange += UpdateUI;
     }
 
-    IEnumerator _VisibilityTimer()
+    void UpdateUI(int health)
     {
-        yield return new WaitForSeconds(0.25f);
+        StartCoroutine(_UpdateUI(health));
+    }
 
-        while (Images[0].color.a > 0)
+    IEnumerator _UpdateUI(int health)
+    {
+        //In case of rapid updated, disables fading of earlier updates (still needs stress-testing)
+        Fading = false;
+        SetBarAlpha(1);
+
+        //Heal
+        if (health > CurrentHealth)
         {
-            SetBars(-0.2f);
-            yield return new WaitForSeconds(0.05f);
+            #region Heal
+            //Sets the heal indicator to the right position
+            HealIndicator.enabled = true;
+            HealIndicator.transform.position = Vector2.Lerp(Bounds[0].position, Bounds[1].position, health / 100f);
+
+            yield return new WaitForSeconds(0.5f);
+            //Moves the damage indicator to the health indicator over time
+            while (HealIndicator.transform.position != HealthIndicator.transform.position)
+            {
+                HealthIndicator.transform.position = Vector2.MoveTowards(
+                    HealthIndicator.transform.position,
+                    HealIndicator.transform.position,
+                    Time.deltaTime * 5);
+
+                yield return new WaitForSeconds(Time.deltaTime * 2.5f);
+            }
+
+            HealIndicator.enabled = false;
+            #endregion
+        }
+        //Damage
+        else if (health < CurrentHealth)
+        {
+            #region Damage
+            //Sets the health indicator to the right position
+            HealthIndicator.transform.position = Vector2.Lerp(Bounds[0].position, Bounds[1].position, health / 100f);
+            DamageIndicator.enabled = true;
+            //Sets the damage indicator to the right position
+            DamageIndicator.transform.position = Vector2.Lerp(Bounds[0].position, Bounds[1].position, CurrentHealth / 100f);
+
+            yield return new WaitForSeconds(0.5f);
+            //Moves the damage indicator to the health indicator over time
+            while(DamageIndicator.transform.position != HealthIndicator.transform.position)
+            {
+                DamageIndicator.transform.position = Vector2.MoveTowards(
+                    DamageIndicator.transform.position,
+                    HealthIndicator.transform.position,
+                    Time.deltaTime * 5);
+
+                yield return new WaitForSeconds(Time.deltaTime * 2.5f);
+            }
+
+            DamageIndicator.enabled = false;
+            #endregion
+        }
+
+        CurrentHealth = health;
+        StartCoroutine(_FadeOut());
+    }
+
+    //Fades out the bar after showing it a bit
+    IEnumerator _FadeOut()
+    {
+        Fading = true;
+        yield return new WaitForSeconds(0.5f);
+
+        while (Fading && Bar[0].color.a > 0)
+        {
+            SetBarAlpha(-0.1f);
+            yield return new WaitForSeconds(0.1f);
         }
     }
     
-    void SetBars(float alpha)
+    //Highers or lowers the oppacity of the healthbar
+    void SetBarAlpha(float alpha)
     {
         Color temp = new Color(0, 0, 0, alpha);
-        foreach (Image i in Images)
+        foreach (Image i in Bar)
         {
             i.color += temp;
         }
