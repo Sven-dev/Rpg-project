c22506507619a2be8fb09352369d56b825a138c8
Unknown <svenjanssen12321@gmail.com>
2019-02-07 22:51:00 +0100

    Camera clamps on edges instead of center
    makes it easier to set bounds instead of trial and error

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -9,14 +9,17 @@ public class CameraMover : MonoBehaviour
     public float Speed = 1f;
 
     private bool Bounded = false;
-    private Transform BottomLeft;
-    private Transform TopRight;
+    private Transform BottomLeftClamp;
+    private Transform TopRightClamp;
+
+    private Vector2 CornerDistance;
 
     // Use this for initialization
     void Start()
     {
         Target = Global.Player;
         SceneManager.activeSceneChanged += SetBounds;
+        SetCornerDistance();
     }
 
     // Update is called once per frame
@@ -40,13 +43,46 @@ public class CameraMover : MonoBehaviour
         }
     }
 
-    //Ensures the camera stays within the bounds of the map
-    void ClampBounds()
+    //Calculates the x and y distance between the center and the corners of the camera 
+    private void SetCornerDistance()
     {
-        transform.position = new Vector2(
-            Mathf.Clamp(transform.position.x, BottomLeft.position.x, TopRight.position.x),
-            Mathf.Clamp(transform.position.y, BottomLeft.position.y, TopRight.position.y)
-            );
+        Vector2 corner = Global.MainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        CornerDistance = new Vector2(transform.position.x - corner.x, transform.position.y - corner.y);
+    }
+
+    //Ensures the camera stays within the bounds of the map
+    private void ClampBounds()
+    {
+        float minxclamp = BottomLeftClamp.position.x + CornerDistance.x;
+        float maxxclamp = TopRightClamp.position.x - CornerDistance.x;
+
+        float minyclamp = BottomLeftClamp.position.y + CornerDistance.y;
+        float maxyclamp = TopRightClamp.position.y - CornerDistance.y;
+
+
+        float x;
+        if (minxclamp >= maxxclamp)
+        {
+            x = Mathf.Clamp(transform.position.x, minxclamp, minxclamp);
+        }
+        else
+        {
+            x = Mathf.Clamp(transform.position.x, minxclamp, maxxclamp);
+        }
+
+        float y;
+        if (minyclamp >= maxyclamp)
+        {
+            y = Mathf.Clamp(transform.position.y, minyclamp, minyclamp);
+        }
+        else
+        {
+            y = Mathf.Clamp(transform.position.y, minyclamp, maxyclamp);
+        }
+
+
+        //Clamp the corners of the camera
+        transform.position = new Vector2(x, y);
     }
 
     //Finds the bound object, ands sets the clamped values
@@ -56,8 +92,8 @@ public class CameraMover : MonoBehaviour
         Transform boundbox = Global.ActiveRoom.transform.GetChild(0);
         if (boundbox != null)
         {
-            BottomLeft = boundbox.GetChild(0);
-            TopRight = boundbox.GetChild(1);
+            BottomLeftClamp = boundbox.GetChild(0);
+            TopRightClamp = boundbox.GetChild(1);
             Bounded = true;
         }
     }
