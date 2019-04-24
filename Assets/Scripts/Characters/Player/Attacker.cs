720e6230811bd66b09a3437d7d44edcd9db1846e
Unknown <svenjanssen12321@gmail.com>
2019-04-24 23:21:39 +0200

    Added left & right attack animations, removed windup

--- Assets/Scripts/Characters/Player/Attacker.cs
+++ Assets/Scripts/Characters/Player/Attacker.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Attacker : MonoBehaviour
 {
     public bool HasSword;
-    public bool WindingUp;
-    public int WindUpFrames;
+    public bool Attacking;
     private Movement Movement;
+
+    private List<Collider2D> Hitboxes;
 
     public delegate void AttackLaunch(Direction direction);
     public event AttackLaunch OnAttackLaunch;
@@ -15,72 +17,34 @@ public class Attacker : MonoBehaviour
     private void Start()
     {
         Movement = transform.parent.GetComponent<Movement>();
-    }
 
-    //Cancels the wind-up
-    public void LetGo()
-    {
-        WindingUp = false;
-    }
-
-    //Wind up the attack
-    public void WindUp()
-    {
-        WindingUp = true;
-        StartCoroutine(_WindUp());
-    }
-
-    private IEnumerator _WindUp()
-    {
-        //Make the player move slower
-        Movement.Speed = Movement.Speed / 2;
-
-        //While the attack is winding up
-        int frames = 0;
-        bool WoundUp = false;
-
-        while (WindingUp)
-        {
-            print("Winding up");
-            //If the player lets the attack go
-            if (WindingUp == false)
-            {
-                break;
-            }
-
-            //Up the windup frames
-            frames++;
-            //If the attack is wound up long enough, set it to ready
-            if (frames == WindUpFrames)
-            {
-                print("Completely wound up");
-                WoundUp = true;
-            }
-            yield return new WaitForSeconds(0.01f);
-        }
-
-        //Set the player speed to normal
-        Movement.Speed = Movement.Speed * 2;
-
-        //If the attack is ready, attack
-        if (WoundUp)
-        {
-            Attack();
-        }
+        Hitboxes = GetComponents<Collider2D>().ToList();
+        print(Hitboxes.Count);
     }
 
     //Launch the attack
-    private void Attack()
+    public void Attack()
     {
         StartCoroutine(_Attack());
     }
 
     private IEnumerator _Attack()
     {
-        OnAttackLaunch(Movement.Direction);
-        print("Attack");
         Movement.Immobile = true;
-        yield return new WaitForSeconds(0.75f);
+        Attacking = true;
+        OnAttackLaunch(Movement.Direction);
+
+        Hitboxes[0].enabled = true;
+        yield return new WaitForSeconds(0.25f);
+        Hitboxes[0].enabled = false;
+        Hitboxes[1].enabled = true;
+        yield return new WaitForSeconds(0.25f);
+        Hitboxes[1].enabled = false;
+        Hitboxes[2].enabled = true;
+        yield return new WaitForSeconds(0.25f);
+        Hitboxes[2].enabled = false;
+
+        Attacking = false;
         Movement.Immobile = false;
     }
 }
